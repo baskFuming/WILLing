3de0e53c1d53a5ca635b2b385cdccd2e0e262d3c
付明 <2384030975@qq.com>
2019-12-06 19:40:34 +0800

    提交

--- app/src/main/java/com/xxx/willing/ui/vote/fragment/VoteItemFragment.java
+++ app/src/main/java/com/xxx/willing/ui/vote/fragment/VoteItemFragment.java
@@ -1,7 +1,10 @@
 package com.xxx.willing.ui.vote.fragment;
 
+import android.content.Context;
+
 import com.xxx.willing.R;
 import com.xxx.willing.base.fragment.BaseFragment;
+import com.xxx.willing.view.ExpandableTextView;
 
 public class VoteItemFragment extends BaseFragment{
 
@@ -14,4 +17,35 @@ public class VoteItemFragment extends BaseFragment{
     protected void initData() {
 
     }
+
+
+
+
+
+//
+//    ExpandableTextView 用法
+//    ExpandableTextView expandableTextView = findViewById(R.id.expanded_text);
+//    int viewWidth = getWindowManager().getDefaultDisplay().getWidth() - dp2px(this, 20f);
+//        expandableTextView.initWidth(viewWidth);
+//        expandableTextView.setMaxLines(3);
+//        expandableTextView.setHasAnimation(true);
+//        expandableTextView.setCloseInNewLine(true);
+//        expandableTextView.setOpenSuffixColor(getResources().getColor(R.color.colorAccent));
+//        expandableTextView.setCloseSuffixColor(getResources().getColor(R.color.colorAccent));
+//        expandableTextView.setOriginalText();
+
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dp2px(Context context, float dpValue) {
+        int res = 0;
+        final float scale = context.getResources().getDisplayMetrics().density;
+        if (dpValue < 0)
+            res = -(int) (-dpValue * scale + 0.5f);
+        else
+            res = (int) (dpValue * scale + 0.5f);
+        return res;
+    }
+
 }

40a08408bd62798b5fd92f8e1f5ba4289ebd7cd7
薛鑫鑫 <mypowerxue@163.com>
2019-12-20 17:03:38 +0800

    xxx

--- app/src/main/java/com/xxx/willing/config/AppConfig.java
+++ app/src/main/java/com/xxx/willing/config/AppConfig.java
@@ -5,7 +5,7 @@ public class AppConfig {
     public static final String APP_ETH_BROWSER = "https://ethplorer.io/";   //ETH浏览器
     public static final String APP_BTC_BROWSER = "https://m.btc.com/";    //比特币浏览器
     public static final String APP_EOS_BROWSER = "https://eospark.com/";    //EOs浏览器
-    public static final String APP_HUOBI_BROWSER = "https://www.huobi.vn/zh-cn/";    //火币
+    public static final String APP_HUOBI_BROWSER = "https://m.huobi.io/zh-cn/";    //火币
     public static final String APP_HCOIN_BROWSER = "https://www.hcoin86.com/app";    //Hcoin
     public static final String APP_Study_BROWSER = "http://edu.kongyixueyuan.com/";    //孔壹学院
 
